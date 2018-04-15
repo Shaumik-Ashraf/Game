e9d8cd3a60a0e4a3e60783897634eb92c7dd339a
Shaumik-Ashraf <a.shaumik@gmail.com>
2018-04-14 22:19:31 -0400

    axe

--- AxeItem.java
+++ AxeItem.java
@@ -2,4 +2,18 @@
 
 public class AxeItem extends GameItem {
 
+	public Axe() {
+		super("Axe");
+	}
+
+	public String describe() {}
+
+	public void characterEffect() {}
+
+	public void equipEffect() {}
+
+	public void unequipEffect() {}
+
+	public void itemEffect() {}
+
 }
